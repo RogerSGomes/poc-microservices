dca59327baec2e0cdda31aded21eac7d85fe7ffa
RogerSGomes <rogersg.dev@gmail.com>
2023-07-02 21:29:48 -0300

    Student auth middleware adicionado

---
+++ student/src/student/middlewares/auth.middleware.js
@@ -0,0 +1,23 @@
+const { rmqServer } = require('../../servers/rmq.server');
+
+class AuthMiddleware {
+  async ensureAuthenticated(req, res) {
+    const token = extractTokenFromHeaders(req.headers);
+
+    const tokenPayload = await rmqServer.executeRPC({
+      message: token,
+      queue: 'ensure_authenticated_queue',
+      replyQueue: 'ensure_authenticated_response_queue',
+      correlationId: token,
+    });
+
+    req.profile = tokenPayload;
+  }
+}
+
+function extractTokenFromHeaders(headers) {
+  const [type, token] = headers.authorization?.split(' ') ?? [];
+  return token ?? '';
+}
+
+module.exports = { AuthMiddleware };
--- student/src/student/student.router.js
+++ student/src/student/student.router.js
@@ -1,16 +1,30 @@
 const { StudentController } = require('./student.controller');
+const { AuthMiddleware } = require('./middlewares');
 
 class StudentRouter {
   constructor(router) {
     this.router = router;
     this.studentController = new StudentController();
+    this.authMiddleware = new AuthMiddleware();
   }
 
   execute() {
-    this.router.post('/', this.studentController.handleCreateStudent);
-    this.router.get('/', this.studentController.handleGetAll);
-    this.router.get('/:student_id', this.studentController.handleGetById);
-    this.router.put('/:student_id', this.studentController.handleUpdateStudent);
+    this.router.post(
+      '/',
+      { onRequest: this.authMiddleware.ensureAuthenticated },
+      this.studentController.handleCreateStudent,
+    );
+    this.router.get('/', { onRequest: this.authMiddleware.ensureAuthenticated }, this.studentController.handleGetAll);
+    this.router.get(
+      '/:student_id',
+      { onRequest: this.authMiddleware.ensureAuthenticated },
+      this.studentController.handleGetById,
+    );
+    this.router.put(
+      '/:student_id',
+      { onRequest: this.authMiddleware.ensureAuthenticated },
+      this.studentController.handleUpdateStudent,
+    );
     this.router.post(`/entrar`, this.studentController.handleSignIn);
     this.router.post('/cadastrar', this.studentController.handleSignUp);
   }
